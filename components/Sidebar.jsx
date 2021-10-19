0b44e4d304f6977d384da63b3d530701eeba7cf1
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-18 22:44:40 -0500

    starring page selection on nav

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
+  const router = useRouter();
   return (
     <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
       <div>
@@ -8,20 +10,27 @@ const Sidebar = () => {
       </div>
 
       <nav className="mt-5 list-none">
-          <li>
-              <Link href="/">
-                  <a className="text-white mb-2 block">
-                      Clientes
-                  </a>
-              </Link>
-          </li>
-          <li>
+        <li className={router.pathname === "/" ? "bg-blue-800 p-2" : "p-2"}>
+          <Link href="/">
+            <a className="text-white block">Clientes</a>
+          </Link>
+        </li>
+        <li
+          className={router.pathname === "/pedidos" ? "bg-blue-800 p-2" : "p-2"}
+        >
           <Link href="/pedidos">
-                  <a className="text-white mb-2 block">
-                      Pedidos
-                  </a>
-              </Link>
-          </li>
+            <a className="text-white block">Pedidos</a>
+          </Link>
+        </li>
+        <li
+          className={
+            router.pathname === "/productos" ? "bg-blue-800 p-2" : "p-2"
+          }
+        >
+          <Link href="/productos">
+            <a className="text-white block">Productos</a>
+          </Link>
+        </li>
       </nav>
     </aside>
   );
