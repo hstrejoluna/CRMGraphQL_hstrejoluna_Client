49e1ed50f4e34704a2f0ab5628a6ea69c3c36f96
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-18 21:53:02 -0500

    add head, tailwind and sidebar component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,25 @@
+import Head from "next/head";
+import Sidebar from "./Sidebar";
+
 const Layout = ({ children }) => {
   return (
     <>
-      <h1>Desde Layout</h1>
+      <Head>
+        <title>CRM - Administración de Clientes</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Sidebar />
       {children}
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout from "../components/Layout";
 
+
+
 const Index = () => (
   <div>
     <Layout>
