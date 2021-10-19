78b44afb9b002dc43b09b3a2d374ad225ab4f65d
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-19 16:29:37 -0500

    show msg when user already signed in

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -15,6 +16,8 @@ const NUEVA_CUENTA = gql`
 `;
 
 const NuevaCuenta = () => {
+  // State para el mensaje
+  const [mensaje, guardarMensaje] = useState(null);
   // Mutation para crear nuevos usuarios
   const [nuevoUsuario] = useMutation(NUEVA_CUENTA);
 
@@ -51,7 +54,11 @@ const NuevaCuenta = () => {
           },
         });
       } catch (error) {
-        console.log(error);
+        guardarMensaje(error.message.replace("GraphQL error: ", ""));
+
+        setTimeout(() => {
+          guardarMensaje(null);
+        }, 3000);
       }
     },
   });
@@ -79,10 +86,18 @@ const NuevaCuenta = () => {
       <p className="font-bold">{formik.errors.password}</p>
     </div>
   );
+  const monstrarMensaje = () => {
+    return (
+      <div className="bg-white animate-pulse py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+        <p>{mensaje}</p>
+      </div>
+    );
+  };
 
   return (
     <>
       <Layout>
+        {mensaje && monstrarMensaje()}
         <h1 className="text-center text-2xl text-white font-light">
           Crear Nueva Cuenta
         </h1>
