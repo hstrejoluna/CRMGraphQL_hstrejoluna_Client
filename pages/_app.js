255b2bb08883cbca9aa72282e20e91ee74a95b02
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-19 12:34:38 -0500

    added apollo/client and config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+const MyApp = ({ Component, pageProps }) => {
+  console.log("Desde _app.js");
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
