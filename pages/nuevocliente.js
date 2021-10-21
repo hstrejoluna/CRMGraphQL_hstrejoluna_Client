67e97caa7bde3a0577dd01df2de894aea3cc15f6
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-21 12:17:40 -0500

    frontend form for new client creation

--- pages/nuevocliente.js
+++ pages/nuevocliente.js
@@ -3,7 +3,89 @@ import Layout from "../components/Layout";
 const NuevoCliente = () => {
   return (
     <Layout>
-      <h1>Nuevo Cliente</h1>
+      <h1 className="text-2xl text-gray-800">Nuevo Cliente</h1>
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-lg">
+          <form className="bg-white shadow-md px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nombre"
+              >
+                Nombre
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                id="nombre"
+                type="text"
+                placeholder="Nombre Cliente"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="apellido"
+              >
+                Apellido
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                id="apellido"
+                type="text"
+                placeholder="Apellido Cliente"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="empresa"
+              >
+                Empresa
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                id="empresa"
+                type="text"
+                placeholder="Empresa Cliente"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                id="email"
+                type="email"
+                placeholder="Email Cliente"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="telefono"
+              >
+                Teléfono
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                id="telefono"
+                type="tel"
+                placeholder="Teléfono Cliente"
+              />
+            </div>
+
+            <input
+              type="submit"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 cursor-pointer"
+              value="Registrar Cliente"
+            />
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 };
