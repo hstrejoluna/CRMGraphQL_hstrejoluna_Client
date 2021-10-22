994949dc29e41f61828610f3e77f6e709685b63f
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-21 20:10:19 -0500

    add formik component

--- pages/editarcliente/[pid].js
+++ pages/editarcliente/[pid].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import { useQuery, gql } from "@apollo/client";
+import { Formik } from "formik";
 
 const OBTENER_CLIENTE = gql`
   query obtenerCliente($id: ID!) {
@@ -27,111 +28,119 @@ const EditarCliente = () => {
     },
   });
 
+  if (loading) return "Cargando...";
+
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800">Editar Cliente</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-lg">
-          <form
-            className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
-            onSubmit={formik.handleSubmit}
-          >
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="nombre"
-              >
-                Nombre
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-                id="nombre"
-                type="text"
-                placeholder="Nombre Cliente"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.nombre}
-              />
-              {errorNombre}
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="apellido"
-              >
-                Apellido
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-                id="apellido"
-                type="text"
-                placeholder="Apellido Cliente"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.apellido}
-              />
-            </div>
-            {errorApellido}
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="empresa"
-              >
-                Empresa
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-                id="empresa"
-                type="text"
-                placeholder="Empresa Cliente"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.empresa}
-              />
-            </div>
-            {errorEmpresa}
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-                id="email"
-                type="email"
-                placeholder="Email Cliente"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-              />
-            </div>
-            {errorEmail}
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="telefono"
-              >
-                Teléfono
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-                id="telefono"
-                type="tel"
-                placeholder="Teléfono Cliente"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.telefono}
-              />
-            </div>
+          <Formik>
+            {(props) => {
+              return (
+                <form
+                  className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+                  onSubmit={props.handleSubmit}
+                >
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="nombre"
+                    >
+                      Nombre
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                      id="nombre"
+                      type="text"
+                      placeholder="Nombre Cliente"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.nombre}
+                    />
+                    {errorNombre}
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="apellido"
+                    >
+                      Apellido
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                      id="apellido"
+                      type="text"
+                      placeholder="Apellido Cliente"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.apellido}
+                    />
+                  </div>
+                  {errorApellido}
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="empresa"
+                    >
+                      Empresa
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                      id="empresa"
+                      type="text"
+                      placeholder="Empresa Cliente"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.empresa}
+                    />
+                  </div>
+                  {errorEmpresa}
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                      id="email"
+                      type="email"
+                      placeholder="Email Cliente"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.email}
+                    />
+                  </div>
+                  {errorEmail}
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="telefono"
+                    >
+                      Teléfono
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                      id="telefono"
+                      type="tel"
+                      placeholder="Teléfono Cliente"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.telefono}
+                    />
+                  </div>
 
-            <input
-              type="submit"
-              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 cursor-pointer"
-              value="Registrar Cliente"
-            />
-          </form>
+                  <input
+                    type="submit"
+                    className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 cursor-pointer"
+                    value="Registrar Cliente"
+                  />
+                </form>
+              );
+            }}
+          </Formik>
         </div>
       </div>
     </Layout>
