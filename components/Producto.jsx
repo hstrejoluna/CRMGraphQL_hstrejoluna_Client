534dfa43448e063d440651565c46c8e71261c9ad
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-23 21:23:22 -0500

    finishing table

--- components/Producto.jsx
+++ components/Producto.jsx
@@ -1,6 +1,63 @@
-const Producto = ({producto}) => {
-  const { nombre } = producto;
-  return <h1>{nombre}</h1>;
+import Link from "next/link";
+import Swal from "sweetalert2";
+import { useMutation } from "@apollo/client";
+import Router from "next/router";
+
+const Producto = ({ producto }) => {
+  const { nombre, precio, existencia, id } = producto;
+  return (
+    <tr>
+      <td className="boder px-4 py-2">{nombre}</td>
+      <td className="boder px-4 py-2">{existencia}</td>
+      <td className="boder px-4 py-2">{precio}</td>
+      <td className="boder px-4 py-2">
+        <button
+          type="button"
+          className="flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+         // onClick={() => confirmarEliminarCliente()}
+        >
+          Eliminar
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 ml-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
+            />
+          </svg>
+        </button>
+      </td>
+      <td className="border px-4 py-2">
+        <button
+          type="button"
+          className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+          //onClick={() => editarCliente()}
+        >
+          Editar
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 ml-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+            />
+          </svg>
+        </button>
+      </td>
+    </tr>
+  );
 };
 
 export default Producto;
