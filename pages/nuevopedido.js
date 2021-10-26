5ef5c1dbd3f41d6338a961aad422a4f9e7ba8baf
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-26 13:38:20 -0500

    created page for new orders

---
+++ pages/nuevopedido.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const NuevoPedido = () => {
+  return (
+    <Layout titulo="Nuevo Pedido">
+      <h1 className="text-2xl text-gray-800">Crear Nuevo Pedido</h1>
+    </Layout>
+  );
+};
+
+export default NuevoPedido;
--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,9 +1,15 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 const Pedidos = () => (
   <div>
-    <Layout>
-      <h1 className="text-2xl text-gray-800">Desde Pedidos</h1>
+    <Layout titulo="Pedidos">
+      <h1 className="text-2xl text-gray-800">Pedidos</h1>
+      <Link href="/nuevopedido">
+        <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white text-sm hover:bg-gray-800 mb-3 uppercase rounded font-bold">
+          Nuevo Pedido
+        </a>
+      </Link>
     </Layout>
   </div>
 );
