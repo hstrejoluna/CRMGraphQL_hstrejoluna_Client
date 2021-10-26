f11df6556579c32705b9899ae86adee674ee966e
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-26 15:07:08 -0500

    useReducer configured

---
+++ context/pedidos/PedidoState.js
@@ -0,0 +1,9 @@
+import { useReducer } from "react";
+import PedidoContext from "./PedidoContext";
+import PedidoReducer from "./PedidoReducer";
+
+import {
+  SELECCIONAR_CLIENTE,
+  SELECCIONAR_PRODUCTO,
+  CANTIDAD_PRODUCTOS,
+} from "../../types";
