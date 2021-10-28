8c60568e1877e0d0b5d1282890ceb32465f2e3f4
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-28 13:03:54 -0500

    refactor Select component

--- components/pedidos/AsignarCliente.jsx
+++ components/pedidos/AsignarCliente.jsx
@@ -2,11 +2,9 @@ import { useState, useEffect, useContext } from "react";
 import Select from "react-select";
 import { useQuery } from "@apollo/client";
 
-
 import PedidoContext from "../../context/pedidos/PedidoContext";
 
 import { OBTENER_CLIENTES_USUARIO } from "../../graphql/queries";
-
 
 const AsignarCliente = () => {
   const [cliente, setCliente] = useState({});
@@ -25,19 +23,16 @@ const AsignarCliente = () => {
     setCliente(cliente);
   };
 
-  // Resultados de la consulta
-  if (loading) return null;
-
-  const { obtenerClientesVendedor } = data;
-
   return (
     <>
       <p className="mt-10 my-2 bg-white border-l-4 border-gray-800 text-gray-700 p-2 text-sm font-bold">
         1.- Asigna un Cliente al Pedido
       </p>
       <Select
-        options={obtenerClientesVendedor}
-        onChange={(opcion) => seleccionarCliente(opcion)}
+        isLoading={loading}
+        className="mt-3"
+        options={loading ? null : data.obtenerClientesVendedor}
+        onChange={(cliente) => seleccionarCliente(cliente)}
         getOptionValue={(opciones) => opciones.id}
         getOptionLabel={(opciones) => opciones.nombre}
         placeholder="Busque o seleccione el cliente"
