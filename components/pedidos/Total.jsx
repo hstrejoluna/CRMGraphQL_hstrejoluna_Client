3da8403dcbc0f70db2faf4d8964f293dde15c0c8
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-28 13:23:36 -0500

    removed to string

--- components/pedidos/Total.jsx
+++ components/pedidos/Total.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import PedidoContext from "../../context/pedidos/PedidoContext";
 
 const Total = () => {
+
   const pedidoContext = useContext(PedidoContext);
   const { total } = pedidoContext 
   console.log("total es: "+total)
@@ -9,7 +10,7 @@ const Total = () => {
   return (
     <div className="flex items-center mt-5 justify-between bg-white p-3 border-solid boder-2 border-gray-400">
       <h2 className="text-gray-800 text-lg">Total a pagar: </h2>
-      ${total.toString()}
+      ${total}
     </div>
   );
 };
